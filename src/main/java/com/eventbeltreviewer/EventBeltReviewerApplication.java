2cf615318a3e1661a2c7d3d5bf9476f32085df6b
mbc103020 <88252534+mbc103020@users.noreply.github.com>
2021-09-15 11:43:56 -0500

    initial commit

---
+++ src/main/java/com/eventbeltreviewer/EventBeltReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.eventbeltreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBeltReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBeltReviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventbeltreviewer/EventBeltReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventbeltreviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBeltReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
