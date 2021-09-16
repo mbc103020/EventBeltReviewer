6047a13d4f0f27d44715e1fb2b22bfe9d253d210
Jazmine Spann <jazminespann101@gmail.com>
2021-09-16 16:29:01 -0500

    UserService: updated

--- src/main/java/com/eventbeltreviewer/services/UserService.java
+++ src/main/java/com/eventbeltreviewer/services/UserService.java
@@ -1,5 +1,35 @@
 package com.eventbeltreviewer.services;
 
-public class UserService {
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.eventbeltreviewer.models.User;
+import com.eventbeltreviewer.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Service
+public class UserService {
+private final UserRepository userRepository;
+private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+	this.userRepository = userRepository;
+	this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+}
+
+public void createUser(User user) { 
+	user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+	userRepository.save(user);
+}
+
+public User findById(Long id) { 
+	Optional<User> optional = userRepository.findById(id);
+if( optional.isPresent() ) {
+	return optional.get();
+} else {
+	return null;
+	
+}
+}
 }
