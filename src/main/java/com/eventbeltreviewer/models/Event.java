43a0f3060596428c61d15c790db1b80231173ee4
mbc103020 <88252534+mbc103020@users.noreply.github.com>
2021-09-17 11:36:42 -0500

    updated user and event model, added userevent model and connected relationships

--- src/main/java/com/eventbeltreviewer/models/Event.java
+++ src/main/java/com/eventbeltreviewer/models/Event.java
@@ -1,12 +1,19 @@
 package com.eventbeltreviewer.models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -14,6 +21,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "events")
@@ -45,6 +54,16 @@ public class Event {
 	@Column
 	@DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
 	private Date updatedAt;
+	
+	//for one to many
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	private User planner;
+	
+	//for many to many
+	@OneToMany(mappedBy="event", fetch=FetchType.LAZY)
+	@JsonIgnore
+	private List<UserEvent> attendees;
 	
 	public Event() {
 		
