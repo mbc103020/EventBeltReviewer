41a5311c3bc47add7990cbfc26410cecf8be6c03
Jazmine Spann <jazminespann101@gmail.com>
2021-09-17 14:38:20 -0500

    updated services for user

--- src/main/java/com/eventbeltreviewer/services/UserService.java
+++ src/main/java/com/eventbeltreviewer/services/UserService.java
@@ -6,30 +6,56 @@ import org.springframework.stereotype.Service;
 
 import com.eventbeltreviewer.models.User;
 import com.eventbeltreviewer.repositories.UserRepository;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class UserService {
 private final UserRepository userRepository;
-private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+public UserService(UserRepository userRepository) {
 	this.userRepository = userRepository;
-	this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 }
 
-public void createUser(User user) { 
-	user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-	userRepository.save(user);
+// register user and hash their password
+public User registerUser(User user) { 
+	String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+	user.setPassword(hashed);
+	return userRepository.save(user);
 }
 
-public User findById(Long id) { 
+//find user by email
+public User findByEmail(String email) { 
+	return userRepository.findByEmail(email);
+}
+
+//find user by id
+public User findUserById(Long id) { 
 	Optional<User> optional = userRepository.findById(id);
-if( optional.isPresent() ) {
-	return optional.get();
-} else {
-	return null;
-	
+	if(optional.isPresent()) {
+		return optional.get();
+	}else {
+		return null;
+	}
+}
+
+
+//authenticate user 
+public boolean authenticateUser(String email, String password) { 
+	 // first find the user by email
+    User user = userRepository.findByEmail(email);
+    // if we can't find it by email, return false
+    if(user == null) {
+        return false;
+    } else {
+        // if the passwords match, return true, else, return false
+        if(BCrypt.checkpw(password, user.getPassword())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 }
-}
+
