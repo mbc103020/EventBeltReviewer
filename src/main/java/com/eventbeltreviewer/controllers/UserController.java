4e3aa9c2d591df6441dc18b3963a33335502b732
Jazmine Spann <jazminespann101@gmail.com>
2021-09-17 21:14:52 -0500

    added loginuser postmaping to usercontroller

--- src/main/java/com/eventbeltreviewer/controllers/UserController.java
+++ src/main/java/com/eventbeltreviewer/controllers/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eventbeltreviewer.models.User;
 import com.eventbeltreviewer.services.UserService;
@@ -35,10 +37,26 @@ if(result.hasErrors()) {
 }
 }
 @RequestMapping("/login")
-public String Login(Model model) { 
-	
+public String Login() { 
 	return "user/login";
 }
+
+ //post login info to check for correct credentials if pass then will redirect if fail will recieve error message 
+@RequestMapping(value="/login", method=RequestMethod.POST)
+public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+    // if the user is authenticated, save their user id in session
+    // else, add error messages and return the login page
+	boolean isAuthenticated = userService.authenticateUser(email, password);
+	if(isAuthenticated) {
+		User u = userService.findByEmail(email);
+		session.setAttribute("userId", u.getId());
+		return "redirect:/home";
+	}else {
+		model.addAttribute("error", "Invalid Credentials");
+		return "loginPage.jsp";
+	}
+}
+
 
 
 }
